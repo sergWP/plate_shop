33aeb88d69905ed2e5b92dfbc1ec185b6fcc8acf
sergWP <kuzmenko.sergei@gmail.com>
2021-10-26 22:56:50 +0300

    box-shadow change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+      inner: 'inset 0 4px 4px 0 rgba(0, 0, 0, 0.2)'
+    },
     extend: {},
   },
   variants: {
